119335b30d744686b40c664c66394f37960a1ea6
Tran Thanh Tam <thanhtam.tran@officience.com>
2026-03-23 20:43:01 +0700

    Revert "Auto-reload when new service worker activates"
    This reverts commit ec4272eed70b2720aac70ea59680240f57c428cc.

--- src/App.tsx
+++ src/App.tsx
@@ -216,15 +216,10 @@ function App({onHome}) {
         }
       });
     });
-    // Register service worker + auto-reload on update
-    if ('serviceWorker' in navigator) {
+    // Register service worker
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
       var basePath = new URL('.', window.location.href).pathname;
       navigator.serviceWorker.register(basePath + 'sw.js').catch(function() {});
-      // When a new SW takes control, reload to get fresh assets
-      var refreshing = false;
-      navigator.serviceWorker.addEventListener('controllerchange', function() {
-        if (!refreshing) { refreshing = true; window.location.reload(); }
-      });
     }
   }, []);
 
